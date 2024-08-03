045111d61d3af4ae4c11673c6ccb53c501cf0189
DenysTry <denystry@gmail.com>
2024-08-02 22:44:26 -0400

    feat:feat dashing and dashing anime

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -4,23 +4,14 @@ using System;
 public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
-    [Export] private AnimationPlayer animPlayerNode;
-    [Export] private Sprite3D spriteNode;
+    [Export] public AnimationPlayer animPlayerNode;
+    [Export] public Sprite3D spriteNode;
+    [Export] public StateMachine stateMachineNode;
 
-    private Vector2 direction = new();
+    public Vector2 direction = new();
 
-    public override void _Ready()
-    {
-        animPlayerNode.Play("Idle");
-    }
-    public override void _PhysicsProcess(double delta)
-    {
-        Velocity = new(direction.X,0,direction.Y);
-        Velocity *= 5;
 
-        MoveAndSlide();
-        Flip();
-    }
+
     public override void _Input(InputEvent @event)
     {
         direction = Input
@@ -29,16 +20,9 @@ public partial class Player : CharacterBody3D
               GameConstants.INPUT_MOVE_RIGHT,
               GameConstants.INPUT_MOVE_FORWARD,
               GameConstants.INPUT_MOVE_BACKWARD);
-        if(direction == Vector2.Zero)
-        {
-            animPlayerNode.Play(GameConstants.ANIM_IDLE);
-        }
-        else
-        {
-            animPlayerNode.Play(GameConstants.ANIM_MOVE);
-        }
+   
     }
-    private void Flip()
+    public void Flip()
     {
         bool isNotMovingHorizontally = Velocity.X == 0;
         if (isNotMovingHorizontally)
